ba5563e0c8fbe3b12c1f3a7d8125fcbd756feff0
DeArvis Troutman <dtroutma@stetson.edu>
2018-02-27 14:34:09 -0500

    Update Fully functional valid move

--- src/handler.php
+++ src/handler.php
@@ -1,30 +1,21 @@
-<html>
-<head>
-    <title>Puzzle</title>
-</head>
-<body>
+
 <?php
 require_once 'puzzle_logic.php';
 session_start();
+$puzzle = new puzzle_logic();
 $expiry = new DateTime('+1 year');
-setcookie('blank_x', '1', $expiry->getTimestamp());  //Location of Blank x
-setcookie('blank_y', '1', $expiry->getTimestamp());  // Location of Blank y
+setcookie('blank_x', 1, $expiry->getTimestamp());  //Location of Blank x
+setcookie('blank_y', 1, $expiry->getTimestamp());  // Location of Blank y
 
 if (isset($_COOKIE['currTable']))
 {
     $delimiter = '^';
     $arr = explode($delimiter, $_COOKIE['currTable']);
+    $_SESSION["array"] = $arr;
 }
-print_r($arr);
-//
-//echo "clicked x is" .$_COOKIE['x']. "<br>";
-//echo "clicked y is" .$_COOKIE['y']. "<br>";
-//echo "blank x is" .$_COOKIE['blank_x']. "<br>";
-//echo "blank y is" .$_COOKIE['blank_y']. "<br>";
+$puzzle->valid_click();
+
 ////echo "array is" .$arr. "<br>";
 
-?>
-</body>
 
-</html>
 
--- src/puzzle_logic.php
+++ src/puzzle_logic.php
@@ -8,35 +8,59 @@
 
 class puzzle_logic
 {
+
     public function __construct()
     {
     }
 
     public function switch_elems($val1, $val2)  // Coordinates for Clicked Elements
     {
+
         return $val1 + $val2;
     }
 
     public function valid_click()
     {
+        $_SESSION["x"] =(int)$_COOKIE['x'];
+        $_SESSION["y"] =(int)$_COOKIE['y'];
+        $_SESSION["bl_x"] =(int)$_COOKIE['blank_x'];
+        $_SESSION["bl_y"] =(int)$_COOKIE['blank_y'];
+
+
+
         // (Right) Check to see if selected cell is to the right of the blank one
-        if($_COOKIE['x'] === $_COOKIE['blank_x'] && $_COOKIE['y'] === ($_COOKIE['blank_y'] + 1)) {return true;}
+        if( $_SESSION["x"] === $_SESSION["bl_x"] && $_SESSION["y"] === ( $_SESSION["bl_y"] + 1)) {
+            $_SESSION["output"] = "Valid Click";
+            echo "<p>Total: " . $_SESSION['output'] . "</p>";
+            return true;}
 
         // (Left) Check to see if selected cell is to the left of the blank one
-        if($_COOKIE['x'] === $_COOKIE['blank_x'] &&  $_COOKIE['y'] ===  ($_COOKIE['blank_y'] - 1)) {return true;}
+        if($_SESSION["x"] === $_SESSION["bl_x"] && $_SESSION["y"] === ( $_SESSION["bl_y"] - 1)) {
+            $_SESSION["output"] = "Valid Click";
+            echo "<p>Total: " . $_SESSION['output'] . "</p>";
+            return true;}
 
         // (Top) Check to see if selected cell is on top of the blank one
-        if($_COOKIE['x'] === ($_COOKIE['blank_x'] - 1) && $_COOKIE['y'] ===  $_COOKIE['blank_y'] ){return true;}
+        if($_SESSION["x"] === ($_SESSION["bl_x"]  - 1) && $_SESSION["y"] ===  $_SESSION["bl_y"] ){
+            $_SESSION["output"] = "Valid Click";
+            echo "<p>Total: " . $_SESSION['output'] . "</p>";
+            return true;}
 
         // (Bottom) Check to see if selected cell is under the blank one
-        if($_COOKIE['x'] === ($_COOKIE['blank_x'] + 1) && $_COOKIE['y'] ===  $_COOKIE['blank_y']) {return true;}
+        if($_SESSION["x"] === ($_SESSION["bl_x"]  + 1) && $_SESSION["y"] ===  $_SESSION["bl_y"] ) {
+            $_SESSION["output"] = "Valid Click";
+            echo "<p>Total: " . $_SESSION['output'] . "</p>";
+            return true;}
 
-        return false;
-    }
+
+        $_SESSION["output"] = "Invalid Click";;
+        echo "<p>Total: " . $_SESSION['output'] . "</p>";
+        return false;}
+
 
     public function scramble()
     {
-
+    return true;
     }
 
     public function check_win()
@@ -45,13 +69,13 @@ class puzzle_logic
 
         $answerSheet = array("1","2","3","8"," ","4","7","6","5");
         $z = 0;
-            if ($arr[$z] !== $answerSheet[$z]) // If cell does not match that in the answer sheet return false
+            if ($_SESSION['array'][$z] !== $answerSheet[$z]) // If cell does not match that in the answer sheet return false
             {
                 return false;
             }
             $z++;
 
-        window.alert("Congratulations You Solved The Puzzle!!!");
+       // window.alert("Congratulations You Solved The Puzzle!!!");
         return true;
     }
 }
